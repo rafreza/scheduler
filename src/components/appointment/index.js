f972aa55ecbdb93f7ac700714c77d398af3551d6
rafreza <rafintokyo@gmail.com>
2021-02-17 15:54:00 -0500

    import header and empty into index

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import "./styles.scss";
 
+import Header from "./Header"
+import Empty from "./Empty"
+
 export default function Appointment(props) {
   return (
     <article className="appointment">
+      <Header time={props.time} />
+      <Empty />
     </article>
   );
 }
