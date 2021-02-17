321e7457f11e3c46f9cdc82ac00e9d661949562a
rafreza <rafintokyo@gmail.com>
2021-02-17 00:45:55 -0500

    add IntervewerListItem comonent, styling and stories

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from "classnames/bind";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem (props) {
+  const InterviewerItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
+  return (
+    <li
+      className={InterviewerItemClass}
+      onClick={() => props.setInterviewer(props.name)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+}
