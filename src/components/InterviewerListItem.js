dd8aabf399ba8e999a3522a7ed46c9e2f47594de
rafreza <rafintokyo@gmail.com>
2021-02-17 01:22:35 -0500

    update clickable event, change pass in components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,7 +10,7 @@ export default function InterviewerListItem (props) {
   return (
     <li
       className={InterviewerItemClass}
-      onClick={() => props.setInterviewer(props.name)}
+      onClick={props.setInterviewer}
     >
       <img
         className="interviewers__item-image"
