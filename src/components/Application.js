95dac45a75b79e312426fbb8fb4fdcfb9f2f98c1
rafreza <rafintokyo@gmail.com>
2021-02-18 00:01:11 -0500

    create helper function, pass tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,6 +6,52 @@ import "components/Application.scss";
 
 import Appointment from "components/appointment"
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+  id: 2,
+  time: "1pm",
+  interview: {
+    student: "Lydia Miller-Jones",
+    interviewer: {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Cohen",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Maria Boucher",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+];
 
 
 const schedule = appointments.map((appointment) => {
@@ -18,7 +64,6 @@ const schedule = appointments.map((appointment) => {
 
 export default function Application(props) {
   const [days, setDays] = useState([]);
-  const [appointments, setAppointments] = useState([]);
 
   useEffect(() => {
     const getDays = `http://localhost:8001/api/days`;
@@ -31,7 +76,6 @@ export default function Application(props) {
 
 
   const [day, setDay] = useState("Monday");
-
   
   return (
     <main className="layout">
---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  const dayAppointments = [];
+  const dayArray = state.days.filter((thisDay) => day === thisDay.name);
+
+  if (dayArray === [] || !day || dayArray[0] === undefined) {
+    return [];
+  }
+
+  const apps = dayArray[0].appointments;
+
+  for (const appointment of Object.values(state.appointments)) {
+    if (apps.includes(appointment.id)) {
+      dayAppointments.push(appointment);
+    }
+  }
+
+  return dayAppointments;
+}
