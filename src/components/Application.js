610bd38a463a9dc5faecc7c40c2358cdc7b967dd
rafreza <rafintokyo@gmail.com>
2021-02-17 23:22:31 -0500

    add get days request, delete hard coded data, import axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,72 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 import DayList from "./DayList";
 import "components/Application.scss";
+
 import Appointment from "components/appointment"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-  id: 2,
-  time: "1pm",
-  interview: {
-    student: "Lydia Miller-Jones",
-    interviewer: {
-      id: 1,
-      name: "Sylvia Palmer",
-      avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Cohen",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Maria Boucher",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-];
 
 
 const schedule = appointments.map((appointment) => {
@@ -78,7 +17,21 @@ const schedule = appointments.map((appointment) => {
 });
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    const getDays = `http://localhost:8001/api/days`;
+    axios.get(getDays).then((response) => {
+      setDays([...response.data]);
+      console.log(response.data);
+    });
+
+  }, [days]);
+
+
   const [day, setDay] = useState("Monday");
+
   
   return (
     <main className="layout">
--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -4,10 +4,7 @@ import "./styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
-import Confirm from './Confirm';
-import Status from './Status';
-import Error from './Error';
-import Form from './Form';
+
 
 export default function Appointment(props) {
   let display;
