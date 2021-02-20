0554af9159109e9504b8fdcfa63063e70d590974
rafreza <rafintokyo@gmail.com>
2021-02-20 18:55:27 -0500

    implement custom hook to add/cancel  appointment

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -4,24 +4,30 @@ import "./styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import Form from "./Form";
 
+import useVisualMode from "../../hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  let display;
-  if (!props.interview) {
-    display = <Empty />; 
-  } else {
-    display = (
-      <Show 
-        student={props.interview.student}
-        interviewer={props.interview.interviewer.name}
-      />
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
     );
-  }
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {display}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
       
     </article>
   );
