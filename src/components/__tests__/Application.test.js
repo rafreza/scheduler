321241bf7f7f1d92100c65f19d75c0f5c6b62d14
rafreza <rafintokyo@gmail.com>
2021-02-24 15:19:00 -0500

    add save rendering test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,7 +4,12 @@ import {
   render,
   cleanup,
   waitForElement,
-  fireEvent
+  fireEvent,
+  prettyDOM,
+  getByText,
+  getByAltText,
+  getAllByTestId,
+  getByPlaceholderText
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -20,4 +25,25 @@ describe("Application", () => {
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
   });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
+  });
+
 });
