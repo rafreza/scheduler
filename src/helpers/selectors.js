809053dc59d7a573229dc6529a408536acb8cb10
rafreza <rafintokyo@gmail.com>
2021-02-27 22:48:41 -0500

    use .find in selectors instead of .fiter method

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,13 @@
 function getAppointmentsForDay(state, day) {
   const dayAppointments = [];
-  const dayArray = state.days.filter((thisDay) => day === thisDay.name);
-
-  if (dayArray === [] || !day || dayArray[0] === undefined) {
+  //filters through the states.days array and creates a new array that only contains the appointments of the said day 
+  const dayArray = state.days.find((thisDay) => day === thisDay.name);
+  
+  if (dayArray === [] || !day || dayArray === undefined) {
     return [];
   }
 
-  const apps = dayArray[0].appointments;
+  const apps = dayArray.appointments;
   
   for (const appointment of Object.values(state.appointments)) {
     if (apps.includes(appointment.id)) {
@@ -17,6 +18,7 @@ function getAppointmentsForDay(state, day) {
   return dayAppointments;
 }
 
+//transforms interview data from api to a returned object that components can now work with
 function getInterview(state, interview) {
   if (interview === null || !interview) {
     return null;
@@ -30,15 +32,16 @@ function getInterview(state, interview) {
 
 }
 
+//similar function to getAppointmentsForDay, where we fetch the interviewers available that day instead
 function getInterviewersForDay(state, day) {
   const interviewerList = [];
-  const interviewDayArray = state.days.filter((thisDay) => day === thisDay.name);
+  const interviewDayArray = state.days.find((thisDay) => day === thisDay.name);
 
-  if (interviewDayArray === [] || !day || interviewDayArray[0] === undefined) {
+  if (interviewDayArray === [] || !day || interviewDayArray === undefined) {
     return [];
   }
 
-  const { interviewers } = interviewDayArray[0];
+  const { interviewers } = interviewDayArray;
 
   for (const interviewer of interviewers) {
     interviewerList.push(state.interviewers[interviewer]);
